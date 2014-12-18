40d7a5f630b5906c2840bbd4f062f382528496c8
andy ccs <andychong2@gmail.com>
2014-12-18 18:17:04 +0800

    Profile Activity UI done

--- app/src/main/java/com/nwk/locopromo/ProfileActivity.java
+++ app/src/main/java/com/nwk/locopromo/ProfileActivity.java
@@ -1,39 +1,81 @@
 package com.nwk.locopromo;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+
+import com.nwk.locopromo.adapter.RedeemHistoryListViewAdapter;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import timber.log.Timber;
 
 
 public class ProfileActivity extends ActionBarActivity {
 
+    @InjectView(R.id.history_list)
+    ListView historyList;
+
+    RedeemHistoryListViewAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_TITLE|ActionBar.DISPLAY_HOME_AS_UP);
+        actionBar.show();
+        actionBar.setTitle("Profile");
+
+        ButterKnife.inject(this);
+
+        adapter = new RedeemHistoryListViewAdapter(this);
+        historyList.setAdapter(adapter);
+
+        initializeData();
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_profile, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private void initializeData() {
+        //TODO remove this part
+        ParseQuery<ParseObject> promotionQuery = ParseQuery.getQuery("Promotion");
+        promotionQuery.include("retail");
+        promotionQuery.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> promotions, ParseException e) {
+                Timber.d("Size: " + promotions.size());
+                if (e == null) {
+                    List<Promotion> promotionList = new ArrayList<Promotion>();
+                    for (ParseObject object : promotions) {
+                        Promotion promotion = new Promotion();
+                        promotion.setId(object.getObjectId());
+                        promotion.setTitle(object.getString("title"));
+                        promotion.setDescription(object.getString("description"));
+                        promotion.setImage(object.getParseFile("image").getUrl());
+                        promotion.setQuantity(object.getInt("quantity"));
+                        promotion.setTimeExpiry(object.getDate("timeExpiry"));
+                        promotion.setDiscountPrice(object.getInt("discountPrice"));
+                        promotion.setOriginalPrice(object.getInt("originalPrice"));
+                        promotion.setPercentage(object.getInt("percentage"));
+                        promotion.setType(object.getInt("type"));
+                        promotion.setRetail(object.getParseObject("retail").getObjectId());
+                        promotionList.add(promotion);
+                    }
+                    adapter.setPromotions(promotionList);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/nwk/locopromo/adapter/RedeemHistoryListViewAdapter.java
@@ -0,0 +1,93 @@
+package com.nwk.locopromo.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.nwk.locopromo.Promotion;
+import com.nwk.locopromo.R;
+import com.nwk.locopromo.RedeemDetailActivity;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+/**
+ * Created by Andy on 12/14/2014.
+ */
+public class RedeemHistoryListViewAdapter extends BaseAdapter {
+    private List<Promotion> promotions;
+    private Context context;
+
+    public RedeemHistoryListViewAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return promotions==null
+                ?0:promotions.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return promotions==null?
+                null:promotions.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ViewHolder viewHolder;
+
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.redeem_history_item, null);
+            viewHolder = new ViewHolder(view);
+            view.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) view.getTag();
+        }
+
+        if (promotions.get(i) != null) {
+            final Promotion promotion = promotions.get(i);
+            viewHolder.title.setText(promotion.getTitle());
+
+            Picasso.with(context)
+                    .load(promotion.getImage())
+                    .into(viewHolder.image);
+        }
+
+        view.setClickable(false);
+
+        return view;
+    }
+
+    public void setPromotions(List<Promotion> promotions) {
+        this.promotions = promotions;
+    }
+
+    public static class ViewHolder {
+        @InjectView(R.id.promotion_image)
+        ImageView image;
+
+        @InjectView(R.id.promotion_title)
+        TextView title;
+
+        public ViewHolder(View view) {
+            ButterKnife.inject(this, view);
+        }
+    }
+}
