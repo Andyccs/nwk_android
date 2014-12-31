a07d6301fe5618470a97bf7659635bfb597bdc06
Andyccs <y4andy@gmail.com>
2014-12-31 21:34:26 +0800

    fix bug where the progress bar in login activity is not shown properly

--- merchant/src/main/java/com/nwk/merchant/LoginActivity.java
+++ merchant/src/main/java/com/nwk/merchant/LoginActivity.java
@@ -10,11 +10,13 @@ import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.nwk.core.api.LoginUtil;
@@ -44,11 +46,11 @@ public class LoginActivity extends Activity{
     @InjectView(R.id.login_form)
     View mProgressView;
 
-    @InjectView(R.id.login_progress)
-    View mLoginFormView;
-
     @InjectView(R.id.sign_in_button)
     Button mEmailSignInButton;
+
+    @InjectView(R.id.main)
+    RelativeLayout main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -152,36 +154,14 @@ public class LoginActivity extends Activity{
     /**
      * Shows the progress UI and hides the login form.
      */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    View progress;
     public void showProgress(final boolean show) {
-        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-        // for very easy animations. If available, use these APIs to fade-in
-        // the progress spinner.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });
-
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mProgressView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-                }
-            });
-        } else {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        if(show && progress==null){
+            progress = LayoutInflater.from(this).inflate(R.layout.loading_with_black_mask,null);
+            main.addView(progress);
+        }else if(!show && progress!=null){
+            main.removeView(progress);
+            progress = null;
         }
     }
 
